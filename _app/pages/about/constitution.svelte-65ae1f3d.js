de9fe945a1447dbec6f21f2d6312b64fc6be93d2
mileswatson <contact@mileswatson.net>
2022-04-15 13:13:12 +0000

    Updated.

---
+++ _app/pages/about/constitution.svelte-65ae1f3d.js
@@ -0,0 +1,17 @@
+import{S as A,i as H,s as P,w as T,x as D,y as j,q as B,o as K,B as O,e as d,t as w,k as y,c as u,a as f,h as $,d as a,m as E,b as m,H as S,g as x,K as r,E as F}from"../../chunks/index-cfa37649.js";import{P as L}from"../../chunks/PageHeader-67602b20.js";import"../../chunks/width-55da6166.js";import"../../chunks/index-ad16d0c5.js";function M(C){let t,n,e,l,c,h,g,v,p,b,_,i,R;return{c(){t=d("div"),n=d("blockquote"),e=w(`The JCR is guided by a Constitution which dictates the composition
+            of the Executive Committee and the responsibilities of its officers
+            to Christ's students. It also governs more mundane procedural
+            matters and the nature of the relationship between the JCR and the
+            College. Although wordy and a little dry in places, the Constitution
+            is a crucial document which enshrines the rights of all Christ's
+            students and ensures that the JCR Exec is held accountable for its
+            actions. It is regularly updated and often undergoes major revisions
+            to suit the changing needs of the Christ's student body.`),l=y(),c=d("h1"),h=d("a"),g=w("Download"),v=y(),p=d("h1"),b=w("Preview"),_=y(),i=d("iframe"),this.h()},l(s){t=u(s,"DIV",{class:!0});var o=f(t);n=u(o,"BLOCKQUOTE",{});var J=f(n);e=$(J,`The JCR is guided by a Constitution which dictates the composition
+            of the Executive Committee and the responsibilities of its officers
+            to Christ's students. It also governs more mundane procedural
+            matters and the nature of the relationship between the JCR and the
+            College. Although wordy and a little dry in places, the Constitution
+            is a crucial document which enshrines the rights of all Christ's
+            students and ensures that the JCR Exec is held accountable for its
+            actions. It is regularly updated and often undergoes major revisions
+            to suit the changing needs of the Christ's student body.`),J.forEach(a),l=E(o),c=u(o,"H1",{});var q=f(c);h=u(q,"A",{href:!0,download:!0});var I=f(h);g=$(I,"Download"),I.forEach(a),q.forEach(a),v=E(o),p=u(o,"H1",{});var k=f(p);b=$(k,"Preview"),k.forEach(a),o.forEach(a),_=E(s),i=u(s,"IFRAME",{title:!0,src:!0,class:!0}),f(i).forEach(a),this.h()},h(){m(h,"href","/doc/constitution.pdf"),m(h,"download",""),m(t,"class","content"),m(i,"title","Constitution"),S(i.src,R="/doc/constitution.pdf")||m(i,"src",R),m(i,"class","svelte-1x2kgdo")},m(s,o){x(s,t,o),r(t,n),r(n,e),r(t,l),r(t,c),r(c,h),r(h,g),r(t,v),r(t,p),r(p,b),x(s,_,o),x(s,i,o)},p:F,d(s){s&&a(t),s&&a(_),s&&a(i)}}}function Q(C){let t,n;return t=new L({props:{current:"/about/constitution",title:"Constitution",description:"The JCR is guided by a Constitution which dictates the structure of the Committee and its responsibilities to Christ's students.",$$slots:{default:[M]},$$scope:{ctx:C}}}),{c(){T(t.$$.fragment)},l(e){D(t.$$.fragment,e)},m(e,l){j(t,e,l),n=!0},p(e,[l]){const c={};l&1&&(c.$$scope={dirty:l,ctx:e}),t.$set(c)},i(e){n||(B(t.$$.fragment,e),n=!0)},o(e){K(t.$$.fragment,e),n=!1},d(e){O(t,e)}}}class N extends A{constructor(t){super(),H(this,t,null,Q,P,{})}}export{N as default};
