3a926803ccb587241b044d50de905b03417c3059
mileswatson <contact@mileswatson.net>
2022-07-03 18:41:36 +0000

    Updated.

---
+++ _app/immutable/pages/about/constitution-and-guidelines.svelte-5cbf41a0.js
@@ -0,0 +1,41 @@
+import{S as de,i as he,s as fe,w as se,x as oe,y as ae,q as re,o as le,B as ue,e as l,t as f,k as m,c as u,a as d,h as c,d as t,m as C,b as T,g as o,K as h,E as ce}from"../../chunks/index-afe72f92.js";import{C as pe}from"../../chunks/Content-8bb5763a.js";import{P as me}from"../../chunks/PageHeader-5bc72b95.js";import"../../chunks/width-432c46c4.js";import"../../chunks/index-ecb96d3f.js";function Ce(q){let i,r,n,a,p,F,_,b,j,I,y,U,P,$,k,A,v,B,D,E,g,K,H,x,z,S,J,L,G,R,w,M;return{c(){i=l("h2"),r=f("\u{1F4DC} Constitution"),n=m(),a=l("p"),p=f(`The JCR is guided by a Constitution which dictates the composition
+            of the Executive Committee and the responsibilities of its officers
+            to Christ's students. It also governs more mundane procedural
+            matters and the nature of the relationship between the JCR and the
+            College. Although wordy and a little dry in places, the Constitution
+            is a crucial document which enshrines the rights of all Christ's
+            students and ensures that the JCR Exec is held accountable for its
+            actions. It is regularly updated and often undergoes major revisions
+            to suit the changing needs of the Christ's student body.`),F=m(),_=l("h4"),b=l("a"),j=f("Download"),I=m(),y=l("h2"),U=f("\u{1F4B8} Financial Conduct & Procedure"),P=m(),$=l("p"),k=f(`This document specifies how the JCR handles its finances. It
+            outlines how the annual budget is made, and explains the oversight
+            role of the "CCSU Finance Committee". Although primarily useful as a
+            'how-to' guide for the JCR Treasurer, it also includes technical
+            obligations for clubs handling revenue, for example. It obliges the
+            JCR Exec to maintain financial transparency for all students by
+            publishing and distributing termly expenditure reports.`),A=m(),v=l("p"),B=f(`The JCR has to conform to this document, so it's accessible here for
+            all students to read!`),D=m(),E=l("h4"),g=l("a"),K=f("Download"),H=m(),x=l("h2"),z=f("\u26F9\uFE0F Society Funding Guidelines"),S=m(),J=l("p"),L=f(`Christ's JCR funds sports clubs and non-sports clubs in accordance
+            with their integral functions. Funding requests from clubs are
+            graded as "high priority", "medium priority", "low priority", or
+            "refused", in an equitable and uniform way. The following guidelines
+            specify how to grade different funding requests, and thereby create
+            a fair overall budget.`),G=m(),R=l("h4"),w=l("a"),M=f("Download"),this.h()},l(e){i=u(e,"H2",{});var s=d(i);r=c(s,"\u{1F4DC} Constitution"),s.forEach(t),n=C(e),a=u(e,"P",{});var N=d(a);p=c(N,`The JCR is guided by a Constitution which dictates the composition
+            of the Executive Committee and the responsibilities of its officers
+            to Christ's students. It also governs more mundane procedural
+            matters and the nature of the relationship between the JCR and the
+            College. Although wordy and a little dry in places, the Constitution
+            is a crucial document which enshrines the rights of all Christ's
+            students and ensures that the JCR Exec is held accountable for its
+            actions. It is regularly updated and often undergoes major revisions
+            to suit the changing needs of the Christ's student body.`),N.forEach(t),F=C(e),_=u(e,"H4",{});var O=d(_);b=u(O,"A",{href:!0,download:!0});var Q=d(b);j=c(Q,"Download"),Q.forEach(t),O.forEach(t),I=C(e),y=u(e,"H2",{});var V=d(y);U=c(V,"\u{1F4B8} Financial Conduct & Procedure"),V.forEach(t),P=C(e),$=u(e,"P",{});var W=d($);k=c(W,`This document specifies how the JCR handles its finances. It
+            outlines how the annual budget is made, and explains the oversight
+            role of the "CCSU Finance Committee". Although primarily useful as a
+            'how-to' guide for the JCR Treasurer, it also includes technical
+            obligations for clubs handling revenue, for example. It obliges the
+            JCR Exec to maintain financial transparency for all students by
+            publishing and distributing termly expenditure reports.`),W.forEach(t),A=C(e),v=u(e,"P",{});var X=d(v);B=c(X,`The JCR has to conform to this document, so it's accessible here for
+            all students to read!`),X.forEach(t),D=C(e),E=u(e,"H4",{});var Y=d(E);g=u(Y,"A",{href:!0,download:!0});var Z=d(g);K=c(Z,"Download"),Z.forEach(t),Y.forEach(t),H=C(e),x=u(e,"H2",{});var ee=d(x);z=c(ee,"\u26F9\uFE0F Society Funding Guidelines"),ee.forEach(t),S=C(e),J=u(e,"P",{});var te=d(J);L=c(te,`Christ's JCR funds sports clubs and non-sports clubs in accordance
+            with their integral functions. Funding requests from clubs are
+            graded as "high priority", "medium priority", "low priority", or
+            "refused", in an equitable and uniform way. The following guidelines
+            specify how to grade different funding requests, and thereby create
+            a fair overall budget.`),te.forEach(t),G=C(e),R=u(e,"H4",{});var ne=d(R);w=u(ne,"A",{href:!0,download:!0});var ie=d(w);M=c(ie,"Download"),ie.forEach(t),ne.forEach(t),this.h()},h(){T(b,"href","/doc/constitution.pdf"),T(b,"download",""),T(g,"href","/doc/financial-conduct.pdf"),T(g,"download",""),T(w,"href","/doc/funding-guidelines.pdf"),T(w,"download","")},m(e,s){o(e,i,s),h(i,r),o(e,n,s),o(e,a,s),h(a,p),o(e,F,s),o(e,_,s),h(_,b),h(b,j),o(e,I,s),o(e,y,s),h(y,U),o(e,P,s),o(e,$,s),h($,k),o(e,A,s),o(e,v,s),h(v,B),o(e,D,s),o(e,E,s),h(E,g),h(g,K),o(e,H,s),o(e,x,s),h(x,z),o(e,S,s),o(e,J,s),h(J,L),o(e,G,s),o(e,R,s),h(R,w),h(w,M)},p:ce,d(e){e&&t(i),e&&t(n),e&&t(a),e&&t(F),e&&t(_),e&&t(I),e&&t(y),e&&t(P),e&&t($),e&&t(A),e&&t(v),e&&t(D),e&&t(E),e&&t(H),e&&t(x),e&&t(S),e&&t(J),e&&t(G),e&&t(R)}}}function be(q){let i,r;return i=new pe({props:{$$slots:{default:[Ce]},$$scope:{ctx:q}}}),{c(){se(i.$$.fragment)},l(n){oe(i.$$.fragment,n)},m(n,a){ae(i,n,a),r=!0},p(n,a){const p={};a&1&&(p.$$scope={dirty:a,ctx:n}),i.$set(p)},i(n){r||(re(i.$$.fragment,n),r=!0)},o(n){le(i.$$.fragment,n),r=!1},d(n){ue(i,n)}}}function ge(q){let i,r;return i=new me({props:{current:"/about/constitution-and-guidelines",title:"Constitution & Guidelines",metaDescription:"The JCR is guided by a Constitution which dictates the structure of the Committee and its responsibilities to Christ's students.",$$slots:{default:[be]},$$scope:{ctx:q}}}),{c(){se(i.$$.fragment)},l(n){oe(i.$$.fragment,n)},m(n,a){ae(i,n,a),r=!0},p(n,[a]){const p={};a&1&&(p.$$scope={dirty:a,ctx:n}),i.$set(p)},i(n){r||(re(i.$$.fragment,n),r=!0)},o(n){le(i.$$.fragment,n),r=!1},d(n){ue(i,n)}}}class Ee extends de{constructor(i){super(),he(this,i,null,ge,fe,{})}}export{Ee as default};
