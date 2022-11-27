0d1d1dc4f27da7a65c3d87457d35187dc4900866
mileswatson <contact@mileswatson.net>
2022-11-27 12:06:43 +0000

    Updated.

--- _app/immutable/chunks/10-526e64fe.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/get-involved/societies/_page.svelte-bdff7c0a.js";export{t as component};
---
+++ _app/immutable/chunks/10-9e9232d5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/get-involved/societies/_page.svelte-934e6681.js";export{t as component};
